90f06b625e3b3e8499b39cda6e9aa4664aad956e
nrweir <nicholas.r.weir@gmail.com>
2018-12-14 12:57:14 -0500

    fixing assert statements for sindex, gdf equality

--- tests/baseeval_test.py
+++ tests/baseeval_test.py
@@ -9,18 +9,18 @@ class TestEvalBase(object):
         """Test instantiation of an EvalBase instance from a file."""
         base_instance = EvalBase(os.path.join(cw_eval.data.data_dir,
                                               'gt.geojson'))
-        gt_gdf = cw_eval.data.gt_gdf()
-        assert base_instance.ground_truth_sindex == gt_gdf.sindex
-        assert base_instance.proposal_gdf == gpd.GeoDataFrame([])
-        assert base_instance.ground_truth_GDF == base_instance.ground_truth_GDF_Edit
+        gdf = cw_eval.data.gt_gdf()
+        assert base_instance.ground_truth_sindex.bounds == gdf.sindex.bounds
+        assert base_instance.proposal_GDF.equals(gpd.GeoDataFrame([]))
+        assert base_instance.ground_truth_GDF.equals(base_instance.ground_truth_GDF_Edit)
 
     def test_init_from_gdf(gdf):
         """Test instantiation of an EvalBase from a pre-loaded GeoDataFrame."""
         gdf = cw_eval.data.gt_gdf()
         base_instance = EvalBase(gdf)
-        assert base_instance.ground_truth_sindex == gdf.sindex
-        assert base_instance.proposal_gdf == gpd.GeoDataFrame([])
-        assert base_instance.ground_truth_GDF == base_instance.ground_truth_GDF_Edit
+        assert base_instance.ground_truth_sindex.bounds == gdf.sindex.bounds
+        assert base_instance.proposal_GDF.equals(gpd.GeoDataFrame([]))
+        assert base_instance.ground_truth_GDF.equals(base_instance.ground_truth_GDF_Edit)
 
     def test_init_empty_geojson(empty_geojson_path):
         """Test instantiation of EvalBase with an empty geojson file."""
