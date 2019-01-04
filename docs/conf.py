d9bda8efddf4757c30c848246bc486604e83ebeb
Nick Weir <nrweir@users.noreply.github.com>
2019-01-04 16:09:43 -0500

    adding v1.0 release metadata (#48)

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ author = 'David Lindenbaum and Nick Weir'
 license = 'Apache 2.0'
 
 # The short X.Y version
-version = '0.3'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
